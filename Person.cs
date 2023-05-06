fcc0b321421c0a037237bdc981f3eb5bc0c01c12
Liam Desmond <liamjdesmond84@gmail.com>
2023-05-06 17:38:49 -0500

    Implementing Interface

--- Person.cs
+++ Person.cs
@@ -7,6 +7,7 @@ namespace CSharp_Inheritance
 {
     public class Person
     {
+        public string Name;
         List<Vehicle> myVehicles = new List<Vehicle>();
 
         public void setVehicles(Vehicle v){
@@ -19,6 +20,26 @@ namespace CSharp_Inheritance
                 Console.WriteLine("The Dude's stuff:");
                 x.GetInfo();
             }
+        }
+
+        public IRideable Transport;
+        public double Miles;
+        public Person(string name, IRideable trans)
+        {
+            Name = name;
+            Transport = trans;
+            Miles = 0;
+        }
+        // Person can make use of the capabilities of their "transport"
+        public void GoSomewhere(double miles)
+        {
+            Transport.Ride(miles);
+            Miles += Transport.DistanceTraveled;
+        }
+        public void GetInfo()
+        {
+            Console.WriteLine($"Name: {Name}");
+            Console.WriteLine($"Miles Traveled: {Miles}");
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -10,6 +10,7 @@ namespace CSharp_Inheritance
             Vehicle vehicle = new Vehicle(4,"Green");
 
             Car car = new Car("Yellow", 69, "Hyundai", "Elantra");
+            Horse wilbur = new Horse("Wilbur", 120);
 
             // vehicle.GetInfo();
 
@@ -17,16 +18,38 @@ namespace CSharp_Inheritance
 
             // car.Drive(100);
 
-            Person dude = new Person();
+            Person dude = new Person("Dude", car);
+            Person walter = new Person("Walter", wilbur);
             dude.setVehicles(vehicle);
             dude.setVehicles(car);
 
-            dude.getVehicles();
+            // dude.getVehicles();
 
             // foreach(var x in vehicles){
 
             //     x.GetInfo();
             // }
+
+
+        IRideable[] variousRideables = new IRideable[]
+            {
+                new Car("Yellow", 76, "Dodge", "Dart"),
+                new Car("Green", 22, "Ford", "Fiesta"),
+                new Horse("Silver Queen", 100),
+                new Horse("Samammish", 75),
+            };
+            // let them all ride!
+            foreach(var rideable in variousRideables)
+            {
+                // each "rideable" object will be able to invoke the common "Ride()" method
+                // however the implementation of "Ride()" will be unique to how each class has chosen to build it
+                rideable.Ride(50);
+            }
+
+
+
+
         }
+
     }
 }
