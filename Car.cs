688661defe34bce2aaa9d2f77f2100db18e72c5f
Liam Desmond <liamjdesmond84@gmail.com>
2023-05-06 17:31:36 -0500

    Implementing Interface

--- Car.cs
+++ Car.cs
@@ -3,13 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace CSharp_Inheritance
+namespace CSharp_Inheritance, IRideable
 {
     
     public class Car: Vehicle
     {
         public string Make;
         public string Model;
+
+        //* we can use DistanceTraveled to be a wrapper for "Odometer"
+        public double DistanceTraveled
+        {
+            get { return Odometer; }
+            set { Odometer = value; }
+        }
+        public void Ride(double distance)
+        {
+            //* here we can choose implement this method however we wish
+            //* as long as we meet the requirements of the method
+            Console.WriteLine("... driving ...");
+            DistanceTraveled += distance;
+        }
 
         public Car(string color, int odem, string make, string model) : base(color, odem){
             Make = make;
